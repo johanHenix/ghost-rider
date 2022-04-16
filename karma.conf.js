f87a96a40196fc835d46a23328a0917ec7230a99
Freddy Silber <freddy.silber@cloudcoach.com>
2022-04-16 13:56:11 -0600

    init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ghost-rider'),
+      dir: require('path').join(__dirname, './coverage/ghost-rider'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- projects/ghost-rider/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ghost-rider
- */
-
-export * from './lib/ghost-rider.service';
-export * from './lib/ghost-rider.component';
-export * from './lib/ghost-rider.module';
