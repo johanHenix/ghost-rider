7d0e0a6175cad8511aa7c81588dd0b950f2222e1
Freddy Silber <freddy.silber@cloudcoach.com>
2022-06-04 03:34:04 -0600

    take some action in lyfe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,9 @@
 import { Component } from '@angular/core';
-import { GhostRiderService, GhostRiderStep } from 'ng-ghost-rider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ghost-rider';
-
-  constructor(
-    private readonly _ghostRiderService: GhostRiderService,
-  ) {
-    this._ghostRiderService.start(
-      'tour',
-      [
-        new GhostRiderStep('firstStep'),
-        new GhostRiderStep('secondStep'),
-        new GhostRiderStep('thirdStep'),
-        new GhostRiderStep('fourthStep'),
-      ]
-    );
-  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { GhostRiderEvent, GhostRiderEventType } from 'ng-ghost-rider';
+import { GhostRiderEvent, GhostRiderEventType, GhostRiderService } from 'ng-ghost-rider';
+import { GhostRiderStep } from 'projects/ng-ghost-rider/src/lib/models/ghost-rider-step.model';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: 'home.component.html',
 	styleUrls: ['home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+	private _subs: Map<string, Subscription> = new Map();
 
 	constructor(
 		private readonly _router: Router,
-	) { }
+		private readonly _ghostRiderService: GhostRiderService,
+	) {
+		/**
+		 * We can kick off here... or do it on an 'action'
+		 */
+		// this.startTour();;
+	}
+
+	ngOnDestroy(): void {
+		this._subs.forEach((sub) => sub.unsubscribe());
+	}
 
 	public handleEvent(event: GhostRiderEvent): void {
 		if (event.type === GhostRiderEventType.Next) {
 			this._router.navigate(['movies']);
 		}
+	}
+
+	public startTour(): void {
+		console.log('starting tour');
+		this._ghostRiderService.start(
+			'tour',
+			[
+				new GhostRiderStep('firstStep'),
+				new GhostRiderStep('secondStep'),
+				new GhostRiderStep('thirdStep'),
+				new GhostRiderStep('fourthStep'),
+			]
+		);
 	}
 }
