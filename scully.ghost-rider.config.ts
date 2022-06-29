5862ffc9b44b36923b6b20e482f3168b715708fc
Freddy Silber <freddy.silber@cloudcoach.com>
2022-06-29 17:57:23 -0600

    Revert "ghost rider scully init"
    This reverts commit 1008408af16674656235b83e6588502e7621f330.

--- scully.ghost-rider.config.ts
+++
@@ -1,8 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-
-export const config: ScullyConfig = {
-	projectRoot: './src',
-	projectName: 'ghost-rider',
-	outDir: './dist/static',
-	routes: {},
-};
