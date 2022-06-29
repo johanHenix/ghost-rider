1008408af16674656235b83e6588502e7621f330
Freddy Silber <freddy.silber@cloudcoach.com>
2022-06-29 17:53:25 -0600

    ghost rider scully init

---
+++ scully.ghost-rider.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+	projectRoot: './src',
+	projectName: 'ghost-rider',
+	outDir: './dist/static',
+	routes: {},
+};
