db6534c55abb2632ef1d73bb9b283d0aee6f724a
freddysilber <freddy.silber@cloudcoach.com>
2022-10-05 21:50:41 -0600

    [chore] - clean up (#27)

--- projects/ng-ghost-rider/src/lib/components/step/ghost-rider-step.component.ts
+++ projects/ng-ghost-rider/src/lib/components/step/ghost-rider-step.component.ts
@@ -1,85 +1,16 @@
 import { ChangeDetectorRef, Component, Inject } from '@angular/core';
-import { GhostRiderNavigation } from '../models/ghost-rider-navigation.model';
-import { GhostRiderStepConfig } from '../models/ghost-rider-step-config.model';
-import { GhostRiderEventSource } from '../models/ghost-rider-step-event.model';
-import { GHOST_RIDER_NAVIGATION } from '../tokens/ghost-rider-navigation.token';
-import { PopoverComponent } from './popover.component';
+import { GhostRiderNavigation } from '../../models/ghost-rider-navigation.model';
+import { GhostRiderStepConfig } from '../../models/ghost-rider-step-config.model';
+import { GhostRiderEventSource } from '../../models/ghost-rider-step-event.model';
+import { GHOST_RIDER_NAVIGATION } from '../../tokens/ghost-rider-navigation.token';
+import { PopoverComponent } from './../popover.component';
 
 /**
  * Component that renders the step that the user sees
  */
 @Component({
 	selector: 'ghost-rider-step',
-	template: `
-		<div
-			class="ghost-rider-popover"
-			[ngClass]="nubbinCls"
-			role="dialog"
-		>
-			<div class="ghost-rider-popover__header">
-				<h2 class="ghost-rider_title">{{ details.title || '&nbsp;' }}</h2>
-				<div
-					(click)="close()"
-					class="ghost-rider_close-button"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						role="img"
-						width="1.3em"
-						height="1.3em"
-						preserveAspectRatio="xMidYMid meet"
-						viewBox="0 0 1024 1024"
-					>
-						<path
-							fill="currentColor"
-							d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496z"
-						/>
-					</svg>
-				</div>
-			</div>
-
-			<div class="ghost-rider-popover__body">
-				<ng-container [cdkPortalOutlet]="contentCmp || contentTpl || contentTextTpl"></ng-container>
-			</div>
-
-			<div class="ghost-rider-popover__footer">
-				<span>
-					Step {{ stepIndex + 1 }} of {{ stepCount }}
-				</span>
-				<div>
-					<button
-						(click)="back()"
-						*ngIf="stepIndex > 0"
-						class="ghost-rider_back-button"
-					>
-						Back
-					</button>
-					<button
-						(click)="next()"
-						*ngIf="!isLastStep"
-						class="ghost-rider_next-button"
-					>
-						Next
-					</button>
-					<button
-						(click)="complete()"
-						*ngIf="isLastStep"
-						class="ghost-rider_complete-button"
-					>
-						Complete
-					</button>
-				</div>
-			</div>
-		</div>
-
-		<ng-template
-			cdkPortal
-			#contentTextTpl="cdkPortal"
-		>
-			{{ contentText }}
-		</ng-template>
-	`,
+	templateUrl: 'ghost-rider-step.component.html',
 	styles: [],
 })
 export class GhostRiderStepComponent extends PopoverComponent {
